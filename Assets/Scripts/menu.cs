fb5b2d9f9663f8e193d901caa67950935bd74a9d
FilipeBem <discordso123@gmail.com>
2025-08-20 22:33:46 -0300

    script menuv1

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public void Jogar()
+    {
+
+    }
+    public void Abrir_opcao()
+    {
+
+    }
+    public void Fechar_opcao()
+    {
+
+    }
+    
+    public void Fechar_jogo()
+    {
+        Debug.Log("Saiu do jogo");
+        Application.Quit();
+    }
+}
