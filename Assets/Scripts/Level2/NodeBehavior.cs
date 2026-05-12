47e18c105c6a31a77424cdaa8168de40c469c2a8
OtavioNB <otavio.berchez@sou.unifal-mg.edu.br>
2026-05-12 18:00:10 -0300

    v1.0.11 (ajustes fase 2 e cutscenes)

--- Assets/Scripts/Level2/NodeBehavior.cs
+++ Assets/Scripts/Level2/NodeBehavior.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NodeBehavior : MonoBehaviour
 {
@@ -33,6 +34,9 @@ public class NodeBehavior : MonoBehaviour
 
     [Tooltip("Lista de sprites, seguir msm padrao para todos")]
     [SerializeField] private Sprite[] nodeSprites;
+
+    [Header("Eventos (Para mensagens e tutoriais)")]
+    public UnityEvent onTurnOn;
 
     // referencias
     private SpriteRenderer spriteRenderer;
@@ -92,14 +96,16 @@ public class NodeBehavior : MonoBehaviour
         // o ultimo nodeSprites deve ser o final para assim seguir a ideia de "enchimento"
     }
 
+    // chamado quando um nó é conectado
     public void handleConnection(int connectionID)
     {
-        // primeiro resolve o caso especifico para nos c4 e c5
-        // o codigo faz mais sentido no caso geral, que fica fora do if
+        // primeiro tenta resolver o caso especifico para nos c4 e c5
+        // o codigo faz mais sentido no caso geral, que fica fora deste if
         if (nodeID == 3 || nodeID == 4)
         {
             Debug.Log("[NÓ] Fazendo conexão em " + name + ", resolvendo caso especial");
             isPositive = true;
+            onTurnOn.Invoke();
             updateCables(0);
             if (!ignoreSprite) updateSprite(1);
             return;
@@ -122,6 +128,8 @@ public class NodeBehavior : MonoBehaviour
         {
             Debug.Log("[NÓ] Com todas conexões ativas, o nó " + name + " agora está ligado");
             isPositive = true;
+            // chama evento externo para mensagens e tutoriais
+            onTurnOn.Invoke();
         }
 
         // updates visuais
